4d71be2e3f50ff45b4714213da79e30e3421ebe6
nicolaswinkler <Nicolas.winkler2@gmail.com>
2016-07-22 20:26:02 +0200

    added a file

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char** argv)
+{
+    std::string input;
+    std::cin >> input;
+
+    std::cerr << input << " statement not yet supported!\n";
+}
+
+
